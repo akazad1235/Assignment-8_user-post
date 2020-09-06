411b4e8503be1efe6df4fbfe5edee724112adfe3
akazad1235 <akazad914@gmail.com>
2020-09-06 21:49:12 +0600

    page not fund added

--- src/Components/NotMatch/NotMatch.js
+++ src/Components/NotMatch/NotMatch.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotMatch = () => {
     return (
         <div>
-            <p>component not match</p>
+            <h3 style={{textAlign:'center'}}>Page <span style={{color:'red'}}>404</span> Not found</h3>
         </div>
     );
 };
